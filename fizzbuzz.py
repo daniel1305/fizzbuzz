63726b17c5ccbfd491755e67278e419b674adef0
simsrent <daniel.jozwik123@gmail.com>
2019-05-14 20:05:06 +0200

    limerick added

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,17 @@
 def twofer(who = 'you'):
     return f'one for {who}, one for me'
 
+def limerick():
+  lim_list = ['Był skrzypek rodem z Prabutów,',
+              'miał nogi za duże do butów.',
+              'Wszystkie go uwierały,',
+              'więc nosił futerały',
+              'od skrzypiec zamiast butów.']
+  for line in lim-list:
+      print(line)
+
+
+
 def fizzbuzz():
     for i in range(1, 101):
         if not i % 3 and not i % 5:
